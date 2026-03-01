4081552a0bfefbf770ffb25f62c25ba47b051cb2
Lexalius276 <ludovic.deblock@gmail.com>
2026-03-01 21:13:47 +0100

    Update app.py

--- app.py
+++ app.py
@@ -158,7 +158,9 @@ def annuler(reservation_id):
     conn.close()
     return jsonify({"success": True})
 
+# Init DB au démarrage (Gunicorn + local)
+init_db()
+
 if __name__ == "__main__":
-    init_db()
     port = int(os.environ.get("PORT", 5000))
     app.run(host="0.0.0.0", port=port, debug=False)
